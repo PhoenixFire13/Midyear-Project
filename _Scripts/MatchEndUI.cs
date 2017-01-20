0f24eae9d86f0def81239f74659d4204ec2e7112
PhoenixFire13 <gaoj@bxscience.edu>
2017-01-19 23:08:08 -0500

    Small Update
    - Updated a bit of UI in Stage scene
    - Other changes are probably just minor name changes or something so
    don't bother. If a problem comes up, I'll probably just be able to tell
    you what to change manually.

---
+++ _Scripts/MatchEndUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MatchEndUI : MonoBehaviour {
+
+    Animator canvasAnim;
+
+    public Text gameEndText;
+    public Text winnerText;
+
+    void OnEnable()
+    {
+        gameEndText.enabled = true;
+        winnerText.enabled = true;
+    }
+
+    void OnDisable()
+    {
+        gameEndText.enabled = false;
+        winnerText.enabled = false;
+    }
+
+}
---
+++ _Scripts/MatchStartUI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MatchStartUI : MonoBehaviour {
+
+    Animator canvasAnim;
+    int countdown;
+
+    public Text gameStartText;
+    public Text countdownText;
+    public Text goText;
+    public float startCountDelay;
+    public float countdownDelay;
+    public float goBlink;
+
+    void OnEnable()
+    {
+        gameStartText.enabled = true;
+        countdownText.enabled = true;
+        goText.enabled = false;
+
+        canvasAnim = GetComponentInParent<Animator>();
+
+        countdown = 3;
+        SetCDText();
+
+        StartCoroutine(CountDown());
+    }
+
+    void OnDisable()
+    {
+        gameStartText.enabled = false;
+        countdownText.enabled = false;
+        goText.enabled = false;
+    }
+
+    void SetCDText()
+    {
+        countdownText.text = countdown + "";
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(startCountDelay);
+        canvasAnim.Play("MatchStart");   // Create condition where players have to be present for game to start
+
+        while (countdown > 0)
+        {
+            yield return new WaitForSeconds(countdownDelay);
+
+            countdown--;
+            SetCDText();
+        }
+
+        canvasAnim.Stop();
+        gameStartText.enabled = false;
+        countdownText.enabled = false;
+
+        goText.enabled = true;
+        yield return new WaitForSeconds(goBlink);
+        goText.enabled = false;
+    }
+
+}
